d02967acb66e356f2fedc01c341db976d537b0d8
Mateus Herrera <44376582+mateusherrera@users.noreply.github.com>
2024-05-13 10:30:15 -0300

    chore: classe e doc

--- README.md
+++ README.md
@@ -1,2 +1,527 @@
-# basic_selenium
- Classe de métodos genéricos para agilizar o desenvolvimento de automações com selenium em Python.
+# Basic Automation
+![Python](https://img.shields.io/badge/python-3670A0?style=for-the-badge&logo=python&labelColor=11111b&color=B5E8E0&logoColor=e0e0e0)
+![Selenium](https://img.shields.io/badge/-selenium-%43B02A?style=for-the-badge&logo=selenium&labelColor=11111b&color=B5E8E0&logoColor=e0e0e0)
+
+A classe `BasicAutomation` fornece métodos para agilizar o desenvolvimento de automações com Selenium.
+
+##
+```
+__init__(self,
+        selenium_initial_site: str,
+        selenium_wait_time: int,
+        chrome_driver_directory: str = None,
+        chrome_driver_file: str = 'chromedriver.exe'
+        )
+```
+
+**Parâmetros:**
+- `selenium_initial_site`: URL do site que o selenium irá inicializar o navegador.
+> Ao instanciar a classe, o navegador já será aberto.
+- `selenium_wait_time`: Timeout que o selenium espera até lançar uma `Exception`, ao esperar algum elemento html.
+- `chrome_driver_directory`: Caminho para a pasta que está o driver do chrome. Caso esse argumento seja omitido, o caminho descrito na variável de ambiente `PyPath` será utilizada.
+- `chrome_driver_file`: Nome do arquivo de driver do chrome. Caso seja omitido, será `chromedriver.exe`.
+
+**Exemplo de uso:**
+
+```python
+automation = Automation(
+    'https://www.google.com.br',
+    5,
+    chrome_driver_directory='caminho/para/o/arquivo',
+    chrome_driver_file='nome_do_arquivo.exe'
+)
+```
+
+## Lista de métodos
+
+### `explicit_wait(wait_time: int) -> None`
+
+Faz um sleep explicito, conforme passado por parâmetro
+
+**Parâmetros:**
+- `wait_time`: Quantidade de tempo em segundos.
+
+**Exemplo de uso:**
+
+```python
+Automation.explicit_wait(10)
+```
+
+### `start_browser(self) -> None`
+
+Esse método abre o navegador no site inicial descrito no atributo do objeto.
+
+**Exemplo de uso:**
+
+```python
+automation = Automation(**params)
+automation.start_browser()
+```
+
+### `close_driver(self) -> None`
+
+Esse método fecha o driver.
+
+**Exemplo de uso:**
+
+```python
+automation = Automation(**params)
+automation.start_browser()
+automation.close_driver()
+```
+
+### `reset_frame(self) -> None`
+
+Esse método altera para o frame inicial da página que o driver controla.
+
+**Exemplo de uso:**
+
+```python
+automation = Automation(**params)
+automation.start_browser()
+automation.reset_frame()
+```
+
+### `switch_frame(self, xpath_nw_frame: str) -> None`
+
+Esse método altera para o frame passado por parâmetro.
+
+**Parâmetros:**
+- `xpath_nw_frame`: XPATH do novo frame.
+
+**Exemplo de uso:**
+
+```python
+automation = Automation(**params)
+automation.start_browser()
+automation.switch_frame('xpath/do/iframe')
+```
+
+### `reset_to_default_window(self) -> None`
+
+Esse método altera para a primeira janela do hadler.
+
+**Exemplo de uso:**
+
+```python
+automation = Automation(**params)
+automation.start_browser()
+automation.reset_to_default_window()
+```
+
+### `goto_last_window_handler(self) -> None`
+
+Esse método altera para a última janela do hadler.
+
+**Exemplo de uso:**
+
+```python
+automation = Automation(**params)
+automation.start_browser()
+automation.goto_last_window_hadler()
+```
+
+### `wait_number_window(self, number_of_window: int) -> None`
+
+Esse método espera a quantidade de janelas abertas pelo driver ser um número específico.
+
+**Parâmetros:**
+- `number_of_window`: Número de janelas a se esperar.
+
+**Exemplo de uso:**
+
+```python
+automation = Automation(**params)
+automation.start_browser()
+automation.wait_number_window(2)
+```
+
+### `goto_window_handler(self, index_window: int) -> None`
+
+Esse método altera para uma janela especifica.
+
+**Parâmetros:**
+- `index_window`: Index da janela a ser alterada.
+
+```python
+automation = Automation(**params)
+automation.start_browser()
+automation.goto_window_handler(1)
+```
+
+### `switch_url(self, nw_endereco: str) -> None`
+
+Esse método muda o endereço aberto no driver do selenium.
+
+**Parâmetros:**
+- `nw_endereco`: Novo endereço.
+
+**Exemplo de uso:**
+
+```python
+automation = Automation(**params)
+automation.start_browser()
+automation.switch_url('https://www.google.com.br')
+```
+
+### `back_to_initial_site(self) -> None`
+
+Esse método volta a página do driver para o endereco inicial.
+
+**Exemplo de uso:**
+
+```python
+automation = Automation(**params)
+automation.start_browser()
+automation.back_to_initial_site()
+```
+
+### `qtde_windows(self) -> int`
+
+Esse método retorna a quantidade de janelas abertas no driver do objeto.
+
+**Retorno:**
+- Retorna quantidade de janelas abertas no driver.
+
+**Exemplo de uso:**
+
+```python
+automation = Automation(**params)
+automation.start_browser()
+qtde_open_windows = automation.qtde_windows()
+```
+
+### `verify_if_exists(self, xpath_to_verify: str) -> None`
+
+Esse método verifica a existencia de um elemento pelo seu xpath.
+
+**Parâmetros:**
+- `xpath_to_verify`: XPATH do elemento.
+
+**Exemplo de uso:**
+
+```python
+automation = Automation(**params)
+automation.start_browser()
+automation.verify_if_exists('xpath/do/elemento')
+```
+
+### `verify_invisibility(self, xpath_to_verify: str) -> None`
+
+Esse método espera a invisibilidade de um elemento (espera ele sumir).
+
+**Parâmetro:**
+- `xpath_to_verify`: XPATH do elemento.
+
+**Exemplo de uso:**
+
+```python
+automation = Automation(**params)
+automation.start_browser()
+automation.verify_invisibility('xpath/do/elemento')
+```
+
+### `click_by_xpath(self, xpath_to_click: str) -> None`
+
+Método genérico que clica em um elemento.
+
+**Parâmetros:**
+- `xpath_to_click`: XPATH do elemento que deve ser clicado.
+
+**Exemplo de uso:**
+
+```python
+automation = Automation(**params)
+automation.start_browser()
+automation.click_by_xpath('xpath/do/elemento')
+```
+
+### `click_clickable_method_by_xpath(self, xpath_to_click: str) -> None`
+
+Método genérico que clica em um elemento. Obs.: método de espera diferente do método 'click_by_xpath'.
+
+**Parâmetros:**
+- `xpath_to_click`: XPATH do elemento que deve ser clicado.
+
+**Exemplo de uso:**
+
+```python
+automation = Automation(**params)
+automation.start_browser()
+automation.click_clickable_method_by_xpath('xpath/do/elemento')
+```
+
+### `select_by_value(self, xpath_to_select: str, value: str) -> None`
+
+Método genérico que seleciona uma option.
+
+**Parâmetros:**
+- `xpath_to_select`: XAPTH do elemento select.
+- `value`: Valor visível do option.
+
+**Exemplo de uso:**
+
+```python
+automation = Automation(**params)
+automation.start_browser()
+automation.select_by_value('xpath/do/elemento', 'nome da opção')
+```
+
+### `send_by_xpath(self, xpath_to_send: str, key_to_send: str) -> None`
+
+Método genérico que envia uma string para um elemento.
+
+**Parâmetros:**
+- `xpath_to_send`: XPATH do elemento que deve ser escrito.
+- `key_to_send`: String que deve ser enviada ao elemento.
+
+**Exemplo de uso:**
+
+```python
+automation = Automation(**params)
+automation.start_browser()
+automation.send_by_xpath('xpath/do/elemento', 'string que deve ser enviada para o elemento')
+```
+
+### `deal_alert(self, confirm=True) -> None`
+
+Esse método confirma um alerta.
+
+**Parâmetros:**
+- `confirm` Indicativo se deve aceitar ou recusar o alerta, o padrão é True (aceitar).
+
+**Exemplo de uso:**
+
+```python
+automation = Automation(**params)
+automation.start_browser()
+automation.deal_alert()
+# automation.deal_alert(confirm=False)
+```
+
+### `press_down_input(self, xpath_to_press: str) -> None`
+
+Método genérico para apertar para baixo em um input.
+
+**Parâmetros:**
+- `xpath_to_press`: String com o caminho a ser enviado o comando.
+
+**Exemplo de uso:**
+
+```python
+automation = Automation(**params)
+automation.start_browser()
+automation.press_down_input('xpath/do/elemento')
+```
+
+### `press_up_input(self, xpath_to_press: str) -> None`
+
+Método genérico para apertar para cima em um input.
+
+**Parâmetros:**
+- `xpath_to_press`: String com o caminho a ser enviado o comando.
+
+**Exemplo de uso:**
+
+```python
+automation = Automation(**params)
+automation.start_browser()
+automation.press_up_input('xpath/do/elemento')
+```
+
+### `press_left_input(self, xpath_to_press: str) -> None`
+
+Método genérico para apertar para esquerda em um input.
+
+**Parâmetros:**
+- `xpath_to_press`: String com o caminho a ser enviado o comando.
+
+**Exemplo de uso:**
+
+```python
+automation = Automation(**params)
+automation.start_browser()
+automation.press_left_input('xpath/do/elemento')
+```
+
+### `press_right_input(self, xpath_to_press: str) -> None`
+
+Método genérico para apertar para direita em um input.
+
+**Parâmetros:**
+- `xpath_to_press`: String com o caminho a ser enviado o comando.
+
+**Exemplo de uso:**
+
+```python
+automation = Automation(**params)
+automation.start_browser()
+automation.press_right_input('xpath/do/elemento')
+```
+
+### `press_tab_input(self, xpath_to_press: str) -> None`
+
+Método genérico para apertar TAB em um input.
+
+**Parâmetros:**
+- `xpath_to_press`: String com o caminho a ser enviado o comando.
+
+**Exemplo de uso:**
+
+```python
+automation = Automation(**params)
+automation.start_browser()
+automation.press_tab_input('xpath/do/elemento')
+```
+
+### `confirm_input(self, xpath_to_confirm: str) -> None`
+
+Método genérico para confirmar input de formulário (Pressiona ENTER).
+
+**Parâmetros:**
+- `xpath_to_confirm`: String com o caminho a ser enviado o comando.
+
+**Exemplo de uso:**
+
+```python
+automation = Automation(**params)
+automation.start_browser()
+automation.confirm_input('xpath/do/elemento')
+```
+
+### `clear_input(self, xpath_to_clear: str) -> None`
+
+Método genérico que limpa um input.
+
+**Parâmetros:**
+- `xpath_to_clear`: String com o caminho a ser limpo.
+
+**Exemplo de uso:**
+
+```python
+automation = Automation(**params)
+automation.start_browser()
+automation.clear_input('xpath/do/elemento')
+```
+
+### `get_attribute_by_xpath(self, xpath_element: str, attribute: str) -> str`
+
+Esse método retorno um dado atributo de um elemento identificado pelo seu XPATH no documento HTML.
+
+**Parâmetros:**
+- `xpath_element`: XPATH do elemento.
+- `attribute`: Nome do atributo que deve ser retornado.
+
+**Retorno:**
+- Contéudo do atributo no elemento do HTML.
+
+**Exemlo de uso:**
+
+```python
+automation = Automation(**params)
+automation.start_browser()
+src_element = automation.get_attribute_by_xpath('xpath/do/elemento', 'src')
+print(src_element)
+```
+
+### `get_element(self, xpath_element: str) -> WebElement`
+
+Esse método retorna o objeto do elemento HTML.
+
+**Parâmetros:**
+- `xpath_element`: XPATH do elemento.
+
+**Retornos:**
+- Objeto do elemento HTML.
+
+**Exemplo de uso:**
+
+```python
+automation = Automation(**params)
+automation.start_browser()
+html_element = automation.get_element('xpath/do/elemento')
+```
+
+### `execute_js(self, script_js: str) -> None`
+
+Esse método executa um scipt em JavaScript passado como parâmetro.
+
+**Parâmetro:**
+- `script_js`: Script JavaScript.
+
+**Exemplo de uso:**
+
+```python
+automation = Automation(**params)
+automation.start_browser()
+js_script = 'script JavaScript'
+automation.execute_js(js_script)
+```
+
+### `get_current_url(self) -> str`
+
+Esse método captura a url atual do navegador.
+
+**Retorno:**
+- URL atual.
+
+**Exemplo de us:**
+
+```python
+automation = Automation(**params)
+automation.start_browser()
+current_url = automation.get_current_url()
+print(current_url)
+```
+
+### `is_browser_open(self) -> bool`
+
+Esse método verifica se o navegador está aberto.
+
+**Retorno:**
+- True se estiver aberto, False, caso contrário.
+
+**Exemplo de uso:**
+
+```python
+automation = Automation(**params)
+automation.start_browser()
+is_open = automation.is_browser_open()
+print(is_open)
+```
+
+### `minimize_browser(self) -> None`
+
+Esse método minimiza o browser.
+
+**Exemplo de uso:**
+
+```python
+automation = Automation(**params)
+automation.start_browser()
+automation.minimize_browser()
+```
+
+### `maximize_browser(self) -> None`
+
+Esse método maximiza o browser.
+
+**Exemplo de uso:**
+
+```python
+automation = Automation(**params)
+automation.start_browser()
+automation.miximize_browser()
+```
+
+### `to_up_browser(self) -> None`
+
+Esse método tenta trazer o browser para frente.
+
+**Exemplo de uso:**
+
+```python
+automation = Automation(**params)
+automation.start_browser()
+automation.to_up_browser()
+```
---
+++ automation/basic_automation.py
@@ -0,0 +1,514 @@
+"""
+Módulo para métodos genéricos que agilizam o desenvolvimento de automações com
+a biblioteca Selenium.
+
+:author: Mateus Herrera Gobetti Borges
+:date: 2024-04-26
+"""
+
+# Selenium
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.support import expected_conditions as ec
+from selenium.webdriver.remote.webelement import WebElement
+from selenium.webdriver.chrome.webdriver import WebDriver
+from selenium.common.exceptions import WebDriverException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium import webdriver
+
+# Bibliotecas
+from os import getenv, path
+from time import sleep
+
+
+class BasicAutomation:
+    """
+    Classe para métodos genéricos que agilizam o desenvolvimento de automações
+    com a biblioteca Selenium.
+    """
+
+    # INI: Fields
+
+    selenium_initial_site: str
+    selenium_wait_time: int
+
+    chrome_driver_path: str
+    driver: WebDriver
+    wait: WebDriverWait
+
+    # END: Fields
+
+    # INI: Constructor
+
+    def __init__(self,
+                 selenium_initial_site: str,
+                 selenium_wait_time: int,
+                 chrome_driver_directory: str = None,
+                 chrome_driver_file: str = 'chromedriver.exe'
+                 ):
+        """
+        Construtor da Classe.
+
+        :param selenium_initial_site: Site inicial a ser aberto.
+        :param selenium_wait_time: Tempo de espera do selenium.
+        :param chrome_driver_directory: Caminho do chromedriver.
+        :param chrome_driver_file: Nome do arquivo chromedriver.
+        """
+
+        if chrome_driver_directory is None:
+            env_path = getenv('PyPath')
+
+            if env_path is None:
+                raise Exception(
+                    'Variável de ambiente "PyPath" não está definida.')
+
+            else:
+                chrome_driver_path = path.join(env_path, chrome_driver_file)
+
+        else:
+            chrome_driver_path = path.join(
+                chrome_driver_directory, chrome_driver_file)
+
+        self.chrome_driver_path = chrome_driver_path
+        self.selenium_wait_time = selenium_wait_time
+        self.selenium_initial_site = selenium_initial_site
+
+        self.driver = None
+        self.wait = None
+        pass
+
+    # END: Constructor
+
+    # INI: Static Methods
+
+    @staticmethod
+    def explicit_wait(wait_time: int) -> None:
+        """
+        Faz um sleep explicito, conforme passado por parâmetro.
+
+        :param wait_time: Quantidade de tempo em segundos.
+        """
+
+        sleep(wait_time)
+        pass
+
+    # END: Static Methods
+
+    # INI: Generic Selenium-based Methods
+
+    def start_browser(self):
+        """
+        Esse método abre o navegador.
+
+        :param url_site: URL do site a ser aberto.
+        """
+
+        # Desabilita popup de senha
+        prefs = {
+            "credentials_enable_service": False,
+            "profile.password_manager_enabled": False
+        }
+
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.add_argument('--log-level=3')
+        chrome_options.add_argument('--disable-logging')
+        chrome_options.add_experimental_option("prefs", prefs)
+        chrome_options.add_experimental_option(
+            "excludeSwitches",
+            ['enable-automation']
+        )
+
+        # Preparando chromedriver
+        service = ChromeService(executable_path=self.chrome_driver_path)
+        self.driver = webdriver.Chrome(service=service, options=chrome_options)
+        self.wait = WebDriverWait(self.driver, self.selenium_wait_time)
+
+        self.driver.get(self.selenium_initial_site)
+        self.maximize_browser()
+        pass
+
+    def close_driver(self):
+        """
+        Esse método fecha o driver.
+        """
+
+        try:
+            self.driver.close()
+            pass
+
+        finally:
+            pass
+
+    def reset_frame(self):
+        """
+        Esse método altera para o frame inicial da página que o driver controla.
+        """
+
+        self.driver.switch_to.default_content()
+        pass
+
+    def switch_frame(self, xpath_nw_frame: str):
+        """
+        Esse método altera para o frame passado por parâmetro.
+
+        :param xpath_nw_frame: XPATH do novo frame.
+        """
+
+        self.wait.until(ec.frame_to_be_available_and_switch_to_it(
+            (By.XPATH, xpath_nw_frame)))
+        pass
+
+    def reset_to_default_window(self):
+        """
+        Esse método altera para a primeira janela do hadler.
+        """
+
+        self.driver.switch_to.window(self.driver.window_handles[0])
+        pass
+
+    def goto_last_window_handler(self):
+        """
+        Esse método altera para a última janela do hadler.
+        """
+
+        self.driver.switch_to.window(self.driver.window_handles[-1])
+        pass
+
+    def wait_number_window(self, number_of_window: int):
+        """
+        Esse método espera a quantidade de janelas abertas pelo driver ser um
+        número específico.
+
+        :param number_of_window: Número de janelas a se esperar.
+        """
+
+        self.wait.until(ec.number_of_windows_to_be(number_of_window))
+        pass
+
+    def goto_window_handler(self, index_window: int):
+        """
+        Esse método altera para uma janela especifica.
+
+        :param index_window: Index da janela a ser alterada.
+        """
+
+        try:
+            self.driver.switch_to.window(
+                self.driver.window_handles[index_window])
+            pass
+
+        except Exception as error:
+            raise error
+
+    def switch_url(self, nw_endereco: str):
+        """
+        Esse método muda o endereço aberto no driver do selenium.
+
+        :param nw_endereco: Novo endereço.
+        """
+
+        self.driver.get(nw_endereco)
+        pass
+
+    def back_to_initial_site(self):
+        """
+        Esse método volta a página do driver para o endereco inicial.
+        """
+
+        self.switch_url(self.selenium_initial_site)
+        pass
+
+    def qtde_windows(self) -> int:
+        """
+        Esse método retorna a quantidade de janelas abertas no driver do objeto.
+
+        :return: Quantidade de janelas no driver.
+        """
+
+        return int(len(self.driver.window_handles))
+
+    def verify_if_exists(self, xpath_to_verify: str):
+        """
+        Esse método verifica a existencia de um elemento pelo seu xpath.
+
+        :param xpath_to_verify: XPATH do elemento.
+        """
+
+        try:
+            self.wait.until(ec.visibility_of_element_located(
+                (By.XPATH, xpath_to_verify)))
+            pass
+
+        except Exception as error:
+            raise error
+
+    def verify_invisibility(self, xpath_to_verify: str):
+        """
+        Esse método espera a invisibilidade de um elemento (espera ele sumir).
+
+        :param xpath_to_verify: XPATH do elemento.
+        """
+
+        self.wait.until(ec.invisibility_of_element_located(
+            (By.XPATH, xpath_to_verify)))
+        pass
+
+    def click_by_xpath(self, xpath_to_click: str):
+        """
+        Método genérico que clica em um elemento.
+
+        :param xpath_to_click: XPATH do elemento que deve ser clicado.
+        """
+
+        self.wait.until(ec.visibility_of_all_elements_located(
+            (By.XPATH, xpath_to_click)))
+        self.driver.find_element(By.XPATH, xpath_to_click).click()
+        pass
+
+    def click_clickable_method_by_xpath(self, xpath_to_click: str):
+        """
+        Método genérico que clica em um elemento.
+        Obs.: método de espera diferente do método 'click_by_xpath'.
+
+        :param xpath_to_click: XPATH do elemento que deve ser clicado.
+        """
+
+        self.wait.until(ec.element_to_be_clickable((By.XPATH, xpath_to_click)))
+        self.driver.find_element(By.XPATH, xpath_to_click).click()
+        pass
+
+    def select_by_value(self, xpath_to_select: str, value: str):
+        """
+        Método genérico que seleciona uma option.
+
+        :param xpath_to_select: XAPTH do elemento select.
+        :param value: Valor visível do option.
+        """
+
+        select = Select(self.driver.find_element(By.XPATH, xpath_to_select))
+        select.select_by_value(value)
+        pass
+
+    def send_by_xpath(self, xpath_to_send: str, key_to_send: str):
+        """
+        Método genérico que envia uma string para um elemento.
+
+        :param xpath_to_send: XPATH do elemento que deve ser escrito.
+        :param key_to_send: String que deve ser enviada ao elemento.
+        """
+
+        self.wait.until(ec.visibility_of_element_located(
+            (By.XPATH, xpath_to_send)))
+        self.driver.find_element(
+            By.XPATH, xpath_to_send).send_keys(key_to_send)
+        pass
+
+    def deal_alert(self, confirm=True):
+        """
+        Esse método confirma um alerta.
+
+        :param confirm: Indicativo se deve aceitar ou recusar o alerta, o padrão
+        é aceitar.
+        """
+
+        try:
+            alert = self.wait.until(ec.alert_is_present())
+
+            if confirm:
+                alert.accept()
+            else:
+                alert.dismiss()
+            pass
+
+        finally:
+            pass
+
+    def press_down_input(self, xpath_to_press: str):
+        """
+        Método genérico para apertar para baixo em um input.
+
+        :param xpath_to_press: String com o caminho a ser enviado o comando.
+        """
+
+        self.wait.until(ec.visibility_of_element_located(
+            (By.XPATH, xpath_to_press)))
+        self.driver.find_element(By.XPATH, xpath_to_press).send_keys(Keys.DOWN)
+        pass
+
+    def press_up_input(self, xpath_to_press: str):
+        """
+        Método genérico para apertar para cima em um input.
+
+        :param xpath_to_press: String com o caminho a ser enviado o comando.
+        """
+
+        self.wait.until(ec.visibility_of_element_located(
+            (By.XPATH, xpath_to_press)))
+        self.driver.find_element(By.XPATH, xpath_to_press).send_keys(Keys.UP)
+        pass
+
+    def press_left_input(self, xpath_to_press: str):
+        """
+        Método genérico para apertar para esquerda em um input.
+
+        :param xpath_to_press: String com o caminho a ser enviado o comando.
+        """
+
+        self.wait.until(ec.visibility_of_element_located(
+            (By.XPATH, xpath_to_press)))
+        self.driver.find_element(By.XPATH, xpath_to_press).send_keys(Keys.LEFT)
+        pass
+
+    def press_right_input(self, xpath_to_press: str):
+        """
+        Método genérico para apertar para direita em um input.
+
+        :param xpath_to_press: String com o caminho a ser enviado o comando.
+        """
+
+        self.wait.until(ec.visibility_of_element_located(
+            (By.XPATH, xpath_to_press)))
+        self.driver.find_element(
+            By.XPATH, xpath_to_press).send_keys(Keys.RIGHT)
+        pass
+
+    def press_tab_input(self, xpath_to_press: str):
+        """
+        Método genérico para apertar TAB em um input.
+
+        :param xpath_to_press: String com o caminho a ser enviado o comando.
+        """
+
+        self.wait.until(ec.visibility_of_element_located(
+            (By.XPATH, xpath_to_press)))
+        self.driver.find_element(By.XPATH, xpath_to_press).send_keys(Keys.TAB)
+        pass
+
+    def confirm_input(self, xpath_to_confirm: str):
+        """
+        Método genérico para confirmar input de formulário (Pressiona ENTER).
+
+        :param xpath_to_confirm: String com o caminho a ser confirmado.
+        """
+
+        self.wait.until(ec.visibility_of_element_located(
+            (By.XPATH, xpath_to_confirm)))
+        self.driver.find_element(
+            By.XPATH, xpath_to_confirm).send_keys(Keys.ENTER)
+        pass
+
+    def clear_input(self, xpath_to_clear: str):
+        """
+        Método genérico que limpa um input.
+
+        :param xpath_to_clear: XPATH do input que deve ser limpo.
+        """
+
+        self.wait.until(ec.visibility_of_element_located(
+            (By.XPATH, xpath_to_clear)))
+        self.driver.find_element(By.XPATH, xpath_to_clear).clear()
+        pass
+
+    def get_attribute_by_xpath(self, xpath_element: str, attribute: str) -> str:
+        """
+        Esse método retorno um dado atributo de um elemento identificado pelo
+        seu XPATH no documento HTML.
+
+        :param xpath_element: XPATH do elemento.
+        :param attribute: Nome do atributo que deve ser retornado.
+
+        :return: Contéudo do atributo no elemento do HTML.
+        """
+
+        self.wait.until(ec.presence_of_element_located(
+            (By.XPATH, xpath_element)))
+
+        return self.driver.find_element(By.XPATH, xpath_element)\
+            .get_attribute(attribute)
+
+    def get_element(self, xpath_element: str) -> WebElement:
+        """
+        Esse método retorna o objeto do elemento HTML.
+
+        :param xpath_element: XPATH do elemento.
+
+        :return: Objeto do elemento HTML.
+        """
+
+        self.wait.until(ec.visibility_of_all_elements_located(
+            (By.XPATH, xpath_element)))
+        return self.driver.find_element(By.XPATH, xpath_element)
+
+    def execute_js(self, script_js: str):
+        """
+        Esse método executa um scipt em JavaScript passado como parâmetro.
+
+        :param script_js: Script JavaScript.
+        """
+
+        self.driver.execute_script(script_js)
+        pass
+
+    def get_current_url(self) -> str:
+        """
+        Esse método captura a url atual do navegador.
+
+        :return: URL atual.
+        """
+
+        url = self.driver.current_url
+        return url
+
+    def is_browser_open(self) -> bool:
+        """
+        Esse método verifica se o navegador está aberto.
+
+        :return: True se estiver aberto, False, caso contrário.
+        """
+
+        try:
+            self.driver.current_window_handle
+            return True
+
+        except WebDriverException:
+            return False
+
+    def minimize_browser(self):
+        """
+        Esse método minimiza o browser.
+        """
+
+        try:
+            self.driver.minimize_window()
+            pass
+
+        finally:
+            pass
+
+    def maximize_browser(self):
+        """
+        Esse método maximiza o browser.
+        """
+
+        try:
+            self.driver.maximize_window()
+            pass
+
+        finally:
+            pass
+
+    def to_up_browser(self):
+        """
+        Esse método tenta trazer o browser para frente.
+        """
+
+        self.minimize_browser()
+        self.maximize_browser()
+        pass
+
+    # END: Generic Selenium-based Methods
+
+    # END: Automation #
+    pass
